c2d5fb21c2ec7281f2bc381f65d234247614da67
袁志泳 <1011495619@qq.com>
2022-04-23 19:24:02 +0800

    js原型链

---
+++ JavaScript/JavaScript深入之从原型到原型链.js
@@ -0,0 +1,36 @@
+/*
+* 一 Person是一个构造函数
+*   1、我们使用new创建了一个实例对象person
+*
+* 二、prototype
+*   1、每个函数都有一个prototype属性 注意只有函数才有prototype
+*
+* 三、函数的prototype属性指向哪里？
+*   1、函数prototype属性指向了一个对象，这个对象是调用该构造函数而创建的实例的原型，也就是列子person，person1，person2
+*
+* 四、什么是原型
+*   1、你可以这样理解：每一个JavaScript对象(null除外)在创建的时候就会与之关联另一个对象，这个对象就是我们所说的原型，每一个对象都会从原型"继承"属性。
+*   2、每一个JavaScript对象除了null以外，创建的时候就会关联另外一个对象，这个对象就是原型，每一个对象都会从原型继承属性
+*
+* 五、constructor
+*   1、Person ===  Person.prototype.constructor true  通过constructor属性可以指向构造函数
+ */
+
+function Person() {
+
+}
+
+var person = new Person();
+person.name = 'moshen';
+console.log(person.name); // moshen
+
+Person.prototype.name = 'Kevin';
+var person1 = new Person();
+var person2 = new Person();
+console.log(person1.name);
+console.log(person2.name);
+console.log(person1.__proto__ === Person.prototype )
+console.log(Person ===  Person.prototype.constructor )
+console.log(person1.__proto__ ===  Person.prototype.constructor.prototype )
+console.log(Object.getPrototypeOf(person1)===Person.prototype)
+console.log(Person.prototype)
